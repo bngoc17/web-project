f66596120cc78e043379bcfe6bf2dcd458241fde
bngoc17 <166744779+bngoc17@users.noreply.github.com>
2024-04-12 19:59:37 +0700

    Update search.php
    navbar

--- web-project/bn/php/search.php
+++ web-project/bn/php/search.php
@@ -12,6 +12,7 @@ $search = $_GET["search"];
     <title>Search result</title>
     <link rel="stylesheet" href="..\css\box.css">
     <link rel="stylesheet" href="..\css\font.css">
+    <link rel="stylesheet" href="..\css\navbar.css">
     <link rel="stylesheet" href="..\css\login.css">
     <link rel="stylesheet" href="..\css\searchlist.css">
     <script src="..\js\login.js"></script>
@@ -29,6 +30,314 @@ $search = $_GET["search"];
             ";
         ?>
     </span>
+    <div class="navbar">
+        <span class="navbar-a">
+            <div class="dropdown"> <span class="text">Cửa hàng</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua hàng</p>
+                            <span class="header1"><b>Mac</b></span> <br>
+                            <span class="header1"><b>iPad</b></span> <br>
+                            <span class="header1"><b>iPhone</b></span> <br>
+                            <span class="header1"><b>Apple Watch</b></span> <br>
+                            <span class="header1"><b>Phụ kiện</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Liên Kết Nhanh</p>
+                            <span class="text"><b>Tình Trạng Đơn Hàng</b></span> <br>
+                            <span class="text"><b>Apple Trade In</b></span> <br>
+                            <span class="text"><b>Tài Chính</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua Tại Cửa Hàng Đặc Biệt</p>
+                            <span class="text"><b>Giáo Dục</b></span> <br>
+                            <span class="text"><b>Doanh Nghiệp</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">Mac</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Tất Cả Mac</p>
+                            <span class="header1"><b>MacBook Air</b></span> <br>
+                            <span class="header1"><b>MacBook Pro</b></span> <br>
+                            <span class="header1"><b>iMac</b></span> <br>
+                            <span class="header1"><b>Mac mini</b></span> <br>
+                            <span class="header1"><b>Mac Studio</b></span> <br>
+                            <span class="header1"><b>Mac Pro</b></span> <br>
+                            <span class="header1"><b>Mac Hình</b></span> <br> <br>
+                            <span class="text">So Sánh Mac</span> <br>
+                            <span class="text">Mac Làm Được Đó</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua Mac</p>
+                            <span class="text"><b>Mua Mac</b></span> <br>
+                            <span class="text"><b>Phụ Kiện Mac</b></span> <br>
+                            <span class="text"><b>Apple Trade In</b></span> <br>
+                            <span class="text"><b>Tài Chính</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về Mac</p>
+                            <span class="text"><b>Hỗ Trợ Mac</b></span> <br>
+                            <span class="text"><b>Apple Care+ Cho Mac</b></span> <br>
+                            <span class="text"><b>MacOS Sonoma</b></span> <br>
+                            <span class="text"><b>Các Ứng Dụng Của Apple</b></span> <br>
+                            <span class="text"><b>Tính Liên Tục</b></span> <br>
+                            <span class="text"><b>IClound+</b></span> <br>
+                            <span class="text"><b>Mac Cho Doanh Nghiệp</b></span> <br>
+                            <span class="text"><b>Giáo Dục</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">iPad</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Tất Cả iPad</p>
+                            <span class="header1"><b>iPad Pro</b></span> <br>
+                            <span class="header1"><b>iPad Air</b></span> <br>
+                            <span class="header1"><b>iPad</b></span> <br>
+                            <span class="header1"><b>iPad mini</b></span> <br>
+                            <span class="header1"><b>Apple Pencil</b></span> <br>
+                            <span class="header1"><b>Bàn Phím</b></span> <br>
+                            <span class="header1"><b>Mac Hình</b></span> <br> <br>
+                            <span class="text">So Sánh iPad</span> <br>
+                            <span class="text">Tại Sao Nên Dùng iPad</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua Mac</p>
+                            <span class="text"><b>Mua iPad</b></span> <br>
+                            <span class="text"><b>Phụ Kiện iPad</b></span> <br>
+                            <span class="text"><b>Apple Trade In</b></span> <br>
+                            <span class="text"><b>Tài Chính</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về Mac</p>
+                            <span class="text"><b>Hỗ Trợ iPad</b></span> <br>
+                            <span class="text"><b>Apple Care+ Cho iPad</b></span> <br>
+                            <span class="text"><b>iPadOS 17</b></span> <br>
+                            <span class="text"><b>Các Ứng Dụng Của Apple</b></span> <br>
+                            <span class="text"><b>IClound+</b></span> <br>
+                            <span class="text"><b>Giáo Dục</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">iPhone</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Tất Cả iPhone</p>
+                            <span class="header1"><b>iPhone Pro</b></span> <br>
+                            <span class="header1"><b>iPhone 15</b></span> <br>
+                            <span class="header1"><b>iPhone 14</b></span> <br>
+                            <span class="header1"><b>iPhone 13</b></span> <br>
+                            <span class="header1"><b>iPhone SE</b></span> <br>
+                            <span class="text">So Sánh iPhone</span> <br>
+                            <span class="text">Chuyển Từ Android</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua iPhone</p>
+                            <span class="text"><b>Mua iPhone</b></span> <br>
+                            <span class="text"><b>Phụ Kiện iPhone</b></span> <br>
+                            <span class="text"><b>Apple Trade In</b></span> <br>
+                            <span class="text"><b>Tài Chính</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về iPhone</p>
+                            <span class="text"><b>Hỗ Trợ iPhone</b></span> <br>
+                            <span class="text"><b>Apple Care+ Cho iPhone</b></span> <br>
+                            <span class="text"><b>iOS 17</b></span> <br>
+                            <span class="text"><b>Các Ứng Dụng Của Apple</b></span> <br>
+                            <span class="text"><b>Quyền Riêng Tư Trên iPhone</b></span> <br>
+                            <span class="text"><b>IClound+</b></span> <br>
+                            <span class="text"><b>Ví, Pay</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">Watch</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Tất Cả Watch</p>
+                            <span class="header1"><b>Apple Watch Series 9</b></span> <br>
+                            <span class="header1"><b>Apple Watch Ultra 2</b></span> <br>
+                            <span class="header1"><b>Apple Watch SE</b></span> <br>
+                            <span class="header1"><b>iApple Watch Nike</b></span> <br>
+                            <span class="text">So Sánh Watch</span> <br>
+                            <span class="text">Tại Sao Nên Dùng Apple Watch</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua Watch</p>
+                            <span class="text"><b>Mua Apple Watch</b></span> <br>
+                            <span class="text"><b>Apple Watch Studio</b></span> <br>
+                            <span class="text"><b>Dây Đeo Apple Watch</b></span> <br>
+                            <span class="text"><b>Phụ Kiện Apple Watch</b></span> <br>
+                            <span class="text"><b>Apple Trade In</b></span> <br>
+                            <span class="text"><b>Tài Chính</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về Watch</p>
+                            <span class="text"><b>Hỗ Trợ Watch</b></span> <br>
+                            <span class="text"><b>Apple Care+</b></span> <br>
+                            <span class="text"><b>2atchOS 10</b></span> <br>
+                            <span class="text"><b>Các Ứng Dụng Của Apple</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">AirPods</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá AirPods</p>
+                            <span class="header1"><b>Khám Phá Tất Cả AirPods</b></span> <br>
+                            <span class="header1"><b>AirPods Pro (thế hệ thứ 2)</b></span> <br>
+                            <span class="header1"><b>AirPods (thế hệ thứ 2)</b></span> <br>
+                            <span class="header1"><b>AirPods (thế hệ thứ 3)</b></span> <br>
+                            <span class="header1"><b>AirPods Max</b></span> <br>
+                            <span class="text">So Sánh AirPods</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua AirPods</p>
+                            <span class="text"><b>Mua AirPods</b></span> <br>
+                            <span class="text"><b>Phụ Kiện AirPods</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về AirPods</p>
+                            <span class="text"><b>Hỗ Trợ AirPods</b></span> <br>
+                            <span class="text"><b>Apple Care+ Cho Tai Nghe</b></span> <br>
+                            <span class="text"><b>Apple Music</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">TV & Nhà</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá TV & Nhà</p>
+                            <span class="header1"><b>Khám Phá TV & Nhà</b></span> <br>
+                            <span class="header1"><b>Apple TV 4K</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua TV & Nhà</p>
+                            <span class="text"><b>Mua Apple TV 4K</b></span> <br>
+                            <span class="text"><b>Mua Phụ Kiện TV & Nhà</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Tìm Hiểu Thêm Về TV & Nhà</p>
+                            <span class="text"><b>Hỗ Trợ Apple TV</b></span> <br>
+                            <span class="text"><b>Apple Care+</b></span> <br>
+                            <span class="text"><b>Các Ứng Dụng Apple TV</b></span> <br>
+                            <span class="text"><b>Apple TV+</b></span> <br>
+                            <span class="text"><b>Apple Music</b></span> <br>
+                            <span class="text"><b>AirPlay</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">Giải Trí</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Giải Trí</p>
+                            <span class="header1"><b>Khám Phá Giải Trí</b></span> <br>
+                            <span class="header1"><b>Apple One</b></span> <br>
+                            <span class="header1"><b>Apple TV+</b></span> <br>
+                            <span class="header1"><b>Apple Music</b></span> <br>
+                            <span class="header1"><b>Apple Arcade</b></span> <br>
+                            <span class="header1"><b>Apple Podcasts</b></span> <br>
+                            <span class="header1"><b>Apple Books</b></span> <br>
+                            <span class="header1"><b>App Store </b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Hỗ trợ</p>
+                            <span class="text"><b>Hỗ trợ Apple TV+</b></span> <br>
+                            <span class="text"><b>Hỗ trợ Apple Music</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">Phụ Kiện</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Mua Phụ Kiện</p>
+                            <span class="header1"><b>Mua Tất Cả Phụ Kiện</b></span> <br>
+                            <span class="header1"><b>Mac</b></span> <br>
+                            <span class="header1"><b>iPad</b></span> <br>
+                            <span class="header1"><b>iPhone</b></span> <br>
+                            <span class="header1"><b>Apple Watch</b></span> <br>
+                            <span class="header1"><b>AirPods</b></span> <br>
+                            <span class="header1"><b>TV & Nhà</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Phụ Kiện</p>
+                            <span class="text"><b>Sản Xuất Bởi Apple</b></span> <br>
+                            <span class="text"><b>Beats by Dr.Dre</b></span> <br>
+                            <span class="text"><b>AirTag</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+        <span class="navbar-a">
+            <div class="dropdown"><span class="text">Hỗ Trợ</span>
+                <div class="dropdown-content">
+                    <div class="hover">
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Khám Phá Hỗ Trợ</p>
+                            <span class="header1"><b>iPhone</b></span> <br>
+                            <span class="header1"><b>Mac</b></span> <br>
+                            <span class="header1"><b>iPad</b></span> <br>
+                            <span class="header1"><b>Watch</b></span> <br>
+                            <span class="header1"><b>AirPods</b></span> <br>
+                            <span class="header1"><b>Music</b></span> <br>
+                            <span class="header1"><b>TV</b></span> <br>
+                            <span class="text">Khám Phá Hỗ Trợ</span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Trợ Giúp</p>
+                            <span class="text"><b>Cộng Đồng</b></span> <br>
+                            <span class="text"><b>Kiểm Tra Bảo Hành</b></span> <br>
+                            <span class="text"><b>Sửa Chữa</b></span> <br>
+                            <span class="text"><b>Liên Hệ Chúng Tôi</b></span> <br>
+                        </div>
+                        <div class="hoverlist">
+                            <p class="text" style="color: gray;">Chủ Đề Hữu Ích</p>
+                            <span class="text"><b>Mua Apple Care+</b></span> <br>
+                            <span class="text"><b>ID Apple & Mật Khẩu</b></span> <br>
+                            <span class="text"><b>Thanh Toán & Gói Đăng Ký</b></span> <br>
+                            <span class="text"><b>Tìm</b></span> <br>
+                            <span class="text"><b>Trợ Năng</b></span> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </span>
+    </div>
     <img src="..\KieuHoa & BaoNgoc_transparent.png" alt="" width="200" height="200" style="position: absolute; margin-left: 85%; top: -5px;">
     <p>
     <form action="search.php" method="get">
