c31cbfeea03419873b100c05de6bab0bb255e689
bngoc17 <166744779+bngoc17@users.noreply.github.com>
2024-04-13 00:06:37 +0700

    Add files via upload

---
+++ web-project/js/login.js
@@ -0,0 +1,8 @@
+function login() {
+  var x = document.getElementById("panel");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
+function logout() {
+  var x = document.getElementById("panel1");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
