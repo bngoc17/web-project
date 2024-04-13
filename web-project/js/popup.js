ac17841b390cab0b5014eadb2043841ab58b5bc7
bngoc17 <166744779+bngoc17@users.noreply.github.com>
2024-04-13 14:34:50 +0700

    Add files via upload

---
+++ web-project/js/popup.js
@@ -0,0 +1,16 @@
+function login() {
+  var x = document.getElementById("panel");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
+function logout() {
+  var x = document.getElementById("panel1");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
+function add() {
+  var x = document.getElementById("box");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
+function search() {
+  var x = document.getElementById("searchbar");
+  if (x.style.display === "none") x.style.display = "block"; else x.style.display = "none";
+}
